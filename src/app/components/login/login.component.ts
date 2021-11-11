071fdcca58987b26e416a011218a78efc7ffa5fa
Israel Hernandez Vazquez <Israel.Hernandez@axity.com>
2021-11-10 20:13:18 -0600

    Se agregan recursos para el proyecto

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  form: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      usuario: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  ingresar(){
+    console.log(this.form);
+  }
+
+
+}
